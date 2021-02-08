b0c053fcee311991f516c005915b47fa7fc612b6
Tammashicks <tammas@uoregon.edu>
2021-02-08 14:38:31 -0800

    Includes an intake interface for getting the matrix

--- FloydPython.py
+++ FloydPython.py
@@ -1,24 +1,61 @@
 """
 This program is so that I don't have to manually do
 a floyd warshal demonstration on every bloody step
-of the matrix.
+of the mat.
 """
+
 
 def printmat(mat: list):
     for sublist in mat:
         print(sublist)
     print("\n\n")
 
-def floyd_warsh(matrix: list, n: int):
+
+def floyd_warsh(mat: list, n: int):
+
+    printmat(mat)
 
     for a in range(n):
         for b in range(n):
             for c in range(n):
-                if (matrix[b][a] is None) or (matrix[a][c] is None):
+                if (mat[b][a] is None) or (mat[a][c] is None):
                     pass
-                elif matrix[b][c] is None:
-                    matrix[b][c] = matrix[b][a] + matrix[a][c]
+                elif mat[b][c] is None:
+                    mat[b][c] = mat[b][a] + mat[a][c]
                     pass
-                elif matrix[b][c] > matrix[b][a] + matrix[a][c]:
-                    matrix[b][c] = matrix[b][a] + matrix[a][c]
-        printmat(matrix)
+                elif mat[b][c] > mat[b][a] + mat[a][c]:
+                    mat[b][c] = mat[b][a] + mat[a][c]
+        printmat(mat)
+
+
+if __name__ == '__main__':
+    val = None
+    while not val:
+        try:
+            val = int(input("Please enter the size of the mat: "))
+        except:
+            print("Oops, that's not a number!")
+
+    mat = []
+    for a in range(val):
+        mat.append([])
+        for b in range(val):
+            mat[a].append(None)
+
+
+    print("Fill the mat by row now.")
+    for a in range(val):
+        for b in range(val):
+            inval = 'oops'
+            while inval == 'oops':
+                try:
+                    inval = input("Please enter a number or None: ").lower()
+                    if inval == 'none':
+                        inval = None
+                    else:
+                        inval = int(inval)
+                    mat[a][b] = inval
+
+                except:
+                    print("oops, that's not a number or None")
+    floyd_warsh(mat, val)
